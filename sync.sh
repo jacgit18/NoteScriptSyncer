75d7bc03fbdfe8c9ccdc9e3bae4468781a223bc8
Joshua Carpentier <jacdevgoo2020@gmail.com>
2023-10-28 22:34:00 -0400

    Update sync.sh
    updated script to take arguments

--- sync.sh
+++ sync.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-
 # Install dependencies
 sudo apt update
 sudo apt install -y git libnotify-bin pulseaudio
 
 notify-send -i "Script Notification" "Checking for changes" // libnotify optional dependencey that sends notification
-#paplay /home/jac/Music/sinister-laugh-140131.wav
+paplay /home/jac/Music/sinister-laugh-140131.wav
 
-echo "Please enter the directory path:"
-read directory
-#directory="/home/jac/DevMindMap/"
+directory="$1"
 
 # Change to the selected directory
 cd "$directory" || exit
 
 gstatus=$(git status --porcelain)
 
-# Check if there are any changes to commit
+# Check if the argument is provided
+
+
 if [[ ${#gstatus} -ne 0 ]]; then
     commit_message="${1:-$gstatus}"
     git add --all
@@ -25,8 +24,9 @@ if [[ ${#gstatus} -ne 0 ]]; then
 
     git pull
     git push
-    notify-send -i "Script Notification" "Saved changes to GitHub"// libnotify optional dependencey that sends notification
+    notify-send -i "Script Notification" "Saved changes to github" // libnotify optional dependencey that sends notification
 fi
+
 
 
 
